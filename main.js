e5027b61d307b3a3c16c4f50e1af40f470d197b3
tgiannotta <t.giannotta92@gmail.com>
2021-11-15 17:15:42 +0100

    Fine esercizio

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-const clickMenu = document.querySelector('.header-right');
-clickMenu.addEventListener('click', function() {
-    alert('funziona');
+const clickMenu = document.querySelector('.header-right > a');
+
+clickMenu.addEventListener('click', 
+    function() {
+        const aproMenu = document.querySelector('.hamburger-menu');
+        aproMenu.classList.add('active');   
+        
+});
+
+const chiudoMenu = document.querySelector('.hamburger-menu > a');
+chiudoMenu.addEventListener('click', 
+function() {
+    const aproMenu = document.querySelector('.hamburger-menu');
+    aproMenu.remove('active');
 });
